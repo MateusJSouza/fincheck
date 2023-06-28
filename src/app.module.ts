10c3f5532a9222499f378254bc6cc26061e30985
MateusJSouza <mateusjdev@gmail.com>
2023-06-28 19:48:56 -0300

    ✨ feat: add new route/refactor token authorization

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { DatabaseModule } from './shared/database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './modules/auth/auth.guard';
 
 @Module({
   imports: [UsersModule, DatabaseModule, AuthModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      // Pedindo para o AuthModule prover para todos um APP_GUARD chamado AuthGuard
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
