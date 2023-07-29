6befc6aef92e1da08db3c52d5bd357a97e8d2470
MateusJSouza <mateusjdev@gmail.com>
2023-07-29 18:23:11 -0300

    ✨ feat: create route bank accounts

---
+++ src/modules/bank-accounts/bank-accounts.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { CreateBankAccountDto } from './dto/create-bank-account.dto';
+import { UpdateBankAccountDto } from './dto/update-bank-account.dto';
+import { BankAccountsRepository } from 'src/shared/database/repositories/bank-accounts.repositories';
+
+@Injectable()
+export class BankAccountsService {
+  constructor(private readonly bankAccountsRepo: BankAccountsRepository) {}
+
+  create(userId: string, createBankAccountDto: CreateBankAccountDto) {
+    const { color, initialBalance, name, type } = createBankAccountDto;
+
+    return this.bankAccountsRepo.create({
+      data: {
+        userId,
+        color,
+        initialBalance,
+        name,
+        type,
+      },
+    });
+  }
+
+  findAll() {
+    return `This action returns all bankAccounts`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} bankAccount`;
+  }
+
+  update(id: number, updateBankAccountDto: UpdateBankAccountDto) {
+    return { updateBankAccountDto };
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} bankAccount`;
+  }
+}
---
+++ src/modules/bank-accounts/dto/create-bank-account.dto.ts
@@ -0,0 +1,26 @@
+import {
+  IsEnum,
+  IsHexColor,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+} from 'class-validator';
+import { BankAccountType } from '../entities/bankAccount';
+
+export class CreateBankAccountDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  initialBalance: number;
+
+  @IsNotEmpty()
+  @IsEnum(BankAccountType)
+  type: BankAccountType;
+
+  @IsString()
+  @IsHexColor()
+  color: string;
+}
