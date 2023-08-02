428fdfd37c3bca745c1d969d6fdc57ad6a772c2e
MateusJSouza <mateusjdev@gmail.com>
2023-08-01 23:08:13 -0300

    ✨ feat: created the login and registration page

--- frontend/src/view/pages/Login/index.tsx
+++ frontend/src/view/pages/Login/index.tsx
@@ -1,5 +1,35 @@
+import { Link } from "react-router-dom";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
 export function Login() {
   return (
-    <h1>Login Page</h1>
+    <>
+      <header className="flex flex-col items-center justify-center gap-4 text-center">
+        <h1 className="text-2xl font-bold leading-8 text-gray-900 tracking-[-1px]">Entre em sua conta</h1>
+
+        <p className="space-x-2">
+          <span className="font-regular tracking-[-0.5px]">Novo por aqui?</span>
+          <Link to="/register" className="text-teal-900 tracking-[-0.5px] font-medium">
+            Crie uma conta
+          </Link>
+        </p>
+      </header>
+
+      <form className="mt-[60px] flex flex-col gap-4">
+        <Input
+          name="email"
+          type="email"
+          placeholder="Email"
+        />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Senha"
+        />
+
+        <Button>Entrar</Button>
+      </form>
+    </>
   )
 }
--- frontend/src/view/pages/Register/index.tsx
+++ frontend/src/view/pages/Register/index.tsx
@@ -1,5 +1,39 @@
+import { Link } from "react-router-dom";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
 export function Register() {
   return (
-    <h1>Register Page</h1>
+    <>
+      <header className="flex flex-col items-center justify-center gap-4 text-center">
+        <h1 className="text-2xl font-bold leading-8 text-gray-900 tracking-[-1px]">Crie sua conta</h1>
+
+        <p className="space-x-2">
+          <span className="font-regular tracking-[-0.5px]">Já possui uma conta?</span>
+          <Link to="/login" className="text-teal-900 tracking-[-0.5px] font-medium">
+            Fazer Login
+          </Link>
+        </p>
+      </header>
+
+      <form className="mt-[60px] flex flex-col gap-4">
+        <Input
+          placeholder="Nome"
+          name="name"
+        />
+
+        <Input
+          placeholder="Email"
+          name="email"
+        />
+
+        <Input
+          placeholder="Senha"
+          name="password"
+        />
+
+        <Button>Criar conta</Button>
+      </form>
+    </>
   )
 }
