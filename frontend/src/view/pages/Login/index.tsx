afc43453050a85006d47d51f94be851855481232
MateusJSouza <mateusjdev@gmail.com>
2023-08-02 19:31:00 -0300

    ✨ feat: adding loading property to button and input components

--- frontend/src/view/pages/Login/index.tsx
+++ frontend/src/view/pages/Login/index.tsx
@@ -4,7 +4,7 @@ import { Button } from "../../components/Button";
 import { useLoginController } from "./useLoginController";
 
 export function Login() {
-  const { handleSubmit, register, errors } = useLoginController()
+  const { handleSubmit, register, errors, isLoading } = useLoginController()
 
   return (
     <>
@@ -37,7 +37,7 @@ export function Login() {
           {...register("password")}
         />
 
-        <Button>Entrar</Button>
+        <Button type="submit" className="mt-2" isLoading={isLoading}>Entrar</Button>
       </form>
     </>
   )
