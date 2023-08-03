715963ad35cb38249182a61a021419ec5003d6f7
MateusJSouza <mateusjdev@gmail.com>
2023-08-03 11:00:07 -0300

    ✨ feat: adding react-query-devtools to our App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,8 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import { Toaster } from 'react-hot-toast';
+
 import { Router } from "./Router";
 import { AuthProvider } from "./app/contexts/AuthContext";
 
@@ -19,6 +22,8 @@ export function App() {
         <Router />
         <Toaster />
       </AuthProvider >
+
+      <ReactQueryDevtools />
     </QueryClientProvider>
   )
 }
