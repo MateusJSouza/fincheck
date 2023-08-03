411ed19a96b5ee07f705f2b96017964d07dac440
MateusJSouza <mateusjdev@gmail.com>
2023-08-03 11:22:09 -0300

    ✨ feat: creating a new interceptor for the api response, setting a delay of 500ms

--- frontend/src/services/httpClient.ts
+++ frontend/src/services/httpClient.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { localStorageKeys } from '../app/config/localStorageKeys';
+import { sleep } from '../app/utils/sleep';
 
 export const httpClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL
@@ -15,3 +16,9 @@ httpClient.interceptors.request.use(async (config) => {
 
   return config;
 });
+
+httpClient.interceptors.response.use(async data => {
+  await sleep(500);
+
+  return data;
+})
