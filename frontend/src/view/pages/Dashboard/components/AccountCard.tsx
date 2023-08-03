24d5d737112968cd8bf818bd820da0ef3e9d6b49
MateusJSouza <mateusjdev@gmail.com>
2023-08-03 19:05:48 -0300

    ✨ feat: created the accountCard component

---
+++ frontend/src/view/pages/Dashboard/components/AccountCard.tsx
@@ -0,0 +1,30 @@
+import { formatCurrency } from "../../../../app/utils/formatCurrency";
+import { CategoryIcon } from "../../../components/icons/categories/CategoryIcon";
+
+interface AccountCardProps {
+  color: string;
+  name: string;
+  balance: number;
+}
+
+export function AccountCard({ color, name, balance }: AccountCardProps) {
+  return (
+    <div
+      className="p-4 bg-white rounded-2xl h-[200px] flex flex-col justify-between border-b-2 border-teal-950"
+      style={{ borderColor: color }}
+    >
+      <div className="">
+        <CategoryIcon type="income" />
+
+        <span className="text-gray-800 font-medium tracking-[-0.5px] mt-4 block">
+          {name}
+        </span>
+      </div>
+
+      <div className="">
+        <span className="text-gray-800 font-medium tracking-[-0.5px] block">{formatCurrency(balance)}</span>
+        <small className="text-sm text-gray-600">Saldo atual</small>
+      </div>
+    </div>
+  )
+}
