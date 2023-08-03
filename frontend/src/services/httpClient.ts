9a61d43f0a06bf00ddf4aab6d9c36ad831f17396
MateusJSouza <mateusjdev@gmail.com>
2023-08-03 10:12:45 -0300

    ✨ feat: setting access token in headers if you have one in localStorage

--- frontend/src/services/httpClient.ts
+++ frontend/src/services/httpClient.ts
@@ -1,5 +1,17 @@
 import axios from 'axios'
+import { localStorageKeys } from '../app/config/localStorageKeys';
 
 export const httpClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL
 })
+
+// Setando o token de acesso caso tenha no localStorage
+httpClient.interceptors.request.use(async (config) => {
+  const accessToken = localStorage.getItem(localStorageKeys.ACCESS_TOKEN);
+
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
+});
