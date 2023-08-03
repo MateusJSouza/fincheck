e98be347e7d3b04b630cfb71b7f6ef1132290ef5
MateusJSouza <mateusjdev@gmail.com>
2023-08-03 08:45:20 -0300

    ✨ feat: saving access token in localStorage

--- frontend/src/app/contexts/AuthContext.tsx
+++ frontend/src/app/contexts/AuthContext.tsx
@@ -1,14 +1,30 @@
-import { ReactNode, createContext } from 'react';
+import { ReactNode, createContext, useCallback, useState } from 'react';
+import { localStorageKeys } from '../config/localStorageKeys';
 
 interface AuthContextValue {
   signedIn: boolean;
+  signin(accessToken: string): void;
 }
 
 export const AuthContext = createContext<AuthContextValue>({} as AuthContextValue);
 
 export function AuthProvider({ children }: { children: ReactNode }) {
+  const [signedIn, setSignedIn] = useState<boolean>(() => {
+    const storedAccessToken = localStorage.getItem(localStorageKeys.ACCESS_TOKEN);
+
+    return !!storedAccessToken;
+  });
+
+  const signin = useCallback((accessToken: string) => {
+    localStorage.setItem(localStorageKeys.ACCESS_TOKEN, accessToken);
+
+    localStorage.getItem(localStorageKeys.ACCESS_TOKEN);
+
+    setSignedIn(true);
+  }, [])
+
   return (
-    <AuthContext.Provider value={{ signedIn: true }}>
+    <AuthContext.Provider value={{ signedIn, signin }}>
       {children}
     </AuthContext.Provider>
   )
