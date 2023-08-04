3a8a7d50077adc18d8762fcb058016cedd381ec3
MateusJSouza <mateusjdev@gmail.com>
2023-08-04 12:50:21 -0300

    ✨ feat: created the transaction page header

--- frontend/src/view/pages/Dashboard/components/Transactions/index.tsx
+++ frontend/src/view/pages/Dashboard/components/Transactions/index.tsx
@@ -1,7 +1,56 @@
+import { SliderOption } from "./SliderOption";
+import { FilterIcon } from "../../../../components/icons/FilterIcon";
+import { TransactionsIcon } from "../../../../components/icons/TransactionsIcon";
+
+import { MONTHS } from "../../../../../app/config/constants";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { ChevronDownIcon } from "@radix-ui/react-icons";
+import { SliderNavigation } from "./SliderNavigation";
+
 export function Transactions() {
   return (
     <div className="bg-gray-100 rounded-2xl w-full h-full px-4 py-8 md:p-10">
-      Transactions
+      <header>
+        <div className="flex justify-between items-center">
+          <button className="flex items-center gap-2">
+            <TransactionsIcon />
+            <span className="text-sm text-gray-800 tracking-[-0.5px] font-medium">Transações</span>
+            <ChevronDownIcon className="text-gray-900" />
+          </button>
+
+          <button>
+            <FilterIcon />
+          </button>
+        </div>
+
+        <div className="mt-6 relative">
+          <Swiper
+            slidesPerView={3}
+            centeredSlides
+          >
+            <SliderNavigation />
+            {MONTHS.map((month, index) => (
+              <SwiperSlide
+                key={month}
+              >
+                {({ isActive }) => (
+                  <SliderOption
+                    index={index}
+                    isActive={isActive}
+                    month={month}
+                  />
+                )}
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </header>
+
+      <main className="">
+        Conteúdo
+      </main>
     </div>
   )
 }
