2748b87fd36686ca8589b5e80737ce91ea8c97a0
MateusJSouza <mateusjdev@gmail.com>
2023-08-04 14:42:22 -0300

    🔧 refactor: separating Accounts business rule in a controller

--- frontend/src/view/pages/Dashboard/components/Accounts/useAccountsController.ts
+++ frontend/src/view/pages/Dashboard/components/Accounts/useAccountsController.ts
@@ -1,13 +1,21 @@
 import { useState } from "react"
 import { useWindowWidth } from "../../../../../app/hooks/useWindowWidth"
+import { useDashboard } from "../DashboardContext/useDashboard";
 
 export function useAccountsController() {
   const windowWidth = useWindowWidth();
+  const { areValuesVisible, toggleValueVisibility } = useDashboard()
 
   const [sliderState, setSliderState] = useState({
     isBeginning: true,
     isEnd: false,
   })
 
-  return { sliderState, setSliderState, windowWidth }
+  return {
+    sliderState,
+    setSliderState,
+    windowWidth,
+    areValuesVisible,
+    toggleValueVisibility
+  }
 }
