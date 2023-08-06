f993026abc97f0a30a7e9f7a2eb9a9628645b5dc
MateusJSouza <mateusjdev@gmail.com>
2023-08-06 13:32:14 -0300

    🚚 chore: renaming radix-ui default import

--- frontend/src/view/components/Modal.tsx
+++ frontend/src/view/components/Modal.tsx
@@ -1,4 +1,4 @@
-import * as Dialog from '@radix-ui/react-dialog';
+import * as RdxDialog from '@radix-ui/react-dialog';
 import { cn } from '../../app/utils/cn';
 import { ReactNode } from 'react';
 import { Cross2Icon } from '@radix-ui/react-icons';
@@ -14,9 +14,9 @@ interface ModalProps {
 
 export function Modal({ className, open, title, onClose, rightAction, children }: ModalProps) {
   return (
-    <Dialog.Root open={open} onOpenChange={onClose}>
-      <Dialog.Portal>
-        <Dialog.Overlay
+    <RdxDialog.Root open={open} onOpenChange={onClose}>
+      <RdxDialog.Portal>
+        <RdxDialog.Overlay
           className={cn(
             'fixed inset-0 bg-black/80 z-50 backdrop-blur-sm',
             'data-[state=open]:animate-overlay-show',
@@ -24,7 +24,7 @@ export function Modal({ className, open, title, onClose, rightAction, children }
           )}
         />
 
-        <Dialog.Content
+        <RdxDialog.Content
           className={cn(
             'fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-6 space-y-10 bg-white rounded-2xl z-[51] shadow-[0px_11px_20px_0px_rgba(0, 0, 0, 0.10)] w-full max-w-[400px] outline-none',
             'data-[state=open]:animate-content-show',
@@ -55,8 +55,8 @@ export function Modal({ className, open, title, onClose, rightAction, children }
           <div>
             {children}
           </div>
-        </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog.Root >
+        </RdxDialog.Content>
+      </RdxDialog.Portal>
+    </RdxDialog.Root >
   )
 }
