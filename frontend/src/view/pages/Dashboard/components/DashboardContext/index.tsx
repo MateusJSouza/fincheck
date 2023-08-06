668fa4e4f76a2e4d4956b6094bd57edfb9163dc8
MateusJSouza <mateusjdev@gmail.com>
2023-08-06 13:26:46 -0300

    ✨ feat: creating the functions to open and close the new account modal based on its state

--- frontend/src/view/pages/Dashboard/components/DashboardContext/index.tsx
+++ frontend/src/view/pages/Dashboard/components/DashboardContext/index.tsx
@@ -3,21 +3,37 @@ import { ReactNode, createContext, useCallback, useState } from "react";
 interface DashboardContextValue {
   areValuesVisible: boolean;
   toggleValueVisibility(): void;
+  openNewAccountModal(): void;
+  closeNewAccountModal(): void;
+  isNewAccountModalOpen: boolean;
 }
 
 export const DashboardContext = createContext({} as DashboardContextValue)
 
 export function DashboardProvider({ children }: { children: ReactNode }) {
   const [areValuesVisible, setAreValuesVisible] = useState(true);
+  const [isNewAccountModalOpen, setIsNewAccountModalOpen] = useState(true);
 
   const toggleValueVisibility = useCallback(() => {
     setAreValuesVisible(prevState => !prevState)
   }, []);
 
+  const openNewAccountModal = useCallback(() => {
+    setIsNewAccountModalOpen(true)
+  }, []);
+
+  const closeNewAccountModal = useCallback(() => {
+    setIsNewAccountModalOpen(false)
+  }, []);
+
   return (
     <DashboardContext.Provider
       value={{
-        areValuesVisible, toggleValueVisibility
+        areValuesVisible,
+        isNewAccountModalOpen,
+        toggleValueVisibility,
+        openNewAccountModal,
+        closeNewAccountModal,
       }}
     >
       {children}
