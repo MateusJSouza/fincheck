4f2f878a830f69eb7727659f25e463a969ba823a
MateusJSouza <mateusjdev@gmail.com>
2023-08-07 20:55:37 -0300

    🚚 chore: removing unused className

--- frontend/src/view/pages/Dashboard/components/Accounts/AccountCard.tsx
+++ frontend/src/view/pages/Dashboard/components/Accounts/AccountCard.tsx
@@ -18,7 +18,7 @@ export function AccountCard({ color, name, balance, type }: AccountCardProps) {
       className="p-4 bg-white rounded-2xl h-[200px] flex flex-col justify-between border-b-2 border-b-teal-950"
       style={{ borderBottomColor: color }}
     >
-      <div className="">
+      <div>
         <BankAccountTypeIcon type={type} />
 
         <span className="text-gray-800 font-medium tracking-[-0.5px] mt-4 block">
@@ -26,7 +26,7 @@ export function AccountCard({ color, name, balance, type }: AccountCardProps) {
         </span>
       </div>
 
-      <div className="">
+      <div>
         <span
           className={cn(
             'text-gray-800 font-medium tracking-[-0.5px] block',
