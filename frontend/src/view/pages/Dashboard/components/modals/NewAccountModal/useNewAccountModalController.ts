5b84e977913db646155483aaeeb2ded556577a89
MateusJSouza <mateusjdev@gmail.com>
2023-08-07 16:41:07 -0300

    ✨ feat: creating schema, using useForm functions and returning controller values

--- frontend/src/view/pages/Dashboard/components/modals/NewAccountModal/useNewAccountModalController.ts
+++ frontend/src/view/pages/Dashboard/components/modals/NewAccountModal/useNewAccountModalController.ts
@@ -1,10 +1,39 @@
-import { useDashboard } from "../../DashboardContext/useDashboard"
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useDashboard } from '../../DashboardContext/useDashboard'
+
+const schema = z.object({
+  initialBalance: z.string().nonempty('Saldo inicial é obrigatório'),
+  name: z.string().nonempty('Nome da conta é obrigatório'),
+  type: z.enum(['INVESTMENT', 'CHECKING', 'CASH']),
+  color: z.string().nonempty('Cor é obrigatória'),
+})
+
+type FormData = z.infer<typeof schema>
 
 export function useNewAccountModalController() {
   const { isNewAccountModalOpen, closeNewAccountModal } = useDashboard()
 
+  const {
+    register,
+    handleSubmit: hookFormSubmit,
+    formState: { errors },
+    control
+  } = useForm<FormData>({
+    resolver: zodResolver(schema)
+  })
+
+  const handleSubmit = hookFormSubmit(async (data) => {
+    console.log(data)
+  })
+
   return {
     isNewAccountModalOpen,
-    closeNewAccountModal
+    closeNewAccountModal,
+    register,
+    errors,
+    handleSubmit,
+    control
   }
 }
