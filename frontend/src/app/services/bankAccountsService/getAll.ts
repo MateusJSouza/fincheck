ac8d617713cc9d978938b19201b98ee1564e9f16
MateusJSouza <mateusjdev@gmail.com>
2023-08-08 13:07:47 -0300

    🔧 refactor: creating a BankAccount interface entity

--- frontend/src/app/services/bankAccountsService/getAll.ts
+++ frontend/src/app/services/bankAccountsService/getAll.ts
@@ -1,13 +1,7 @@
+import { BankAccount } from '../../entities/BankAccount';
 import { httpClient } from '../httpClient';
 
-type BankAccountResponse = Array<{
-  id: string;
-  name: string;
-  initialBalance: number;
-  type: 'CHECKING' | 'INVESTMENT' | 'CASH';
-  color: string;
-  currentBalance: number;
-}>
+type BankAccountResponse = Array<BankAccount>
 
 export async function getAll() {
   const { data } = await httpClient.get<BankAccountResponse>('/bank-accounts');
