b5beab873c8f8cd3a5371258ec069ce07e29307a
MateusJSouza <mateusjdev@gmail.com>
2023-08-09 10:40:28 -0300

    ✨ feat: creating the new transaction modal schema with zod and useForm

--- frontend/src/view/pages/Dashboard/components/modals/NewTransactionModal/useNewTransactionModalController.ts
+++ frontend/src/view/pages/Dashboard/components/modals/NewTransactionModal/useNewTransactionModalController.ts
@@ -1,11 +1,45 @@
+import { z } from "zod"
 import { useDashboard } from "../../DashboardContext/useDashboard"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useBankAccounts } from "../../../../../../app/hooks/useBankAccounts";
+
+const schema = z.object({
+  value: z.string().nonempty('Informe o valor'),
+  name: z.string().nonempty('Informe o nome'),
+  categoryId: z.string().nonempty('Informe a categoria'),
+  bankAccountId: z.string().nonempty('Informe a conta'),
+  date: z.date()
+})
+
+type FormData = z.infer<typeof schema>;
 
 export function useNewTransactionModalController() {
   const { isNewTransactionModalOpen, closeNewTransactionModal, newTransactionType, } = useDashboard()
 
+  const {
+    register,
+    handleSubmit: hookFormSubmit,
+    formState: { errors },
+    control,
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  })
+
+  const handleSubmit = hookFormSubmit(data => {
+    console.log(data)
+  })
+
+  const { accounts } = useBankAccounts();
+
   return {
     isNewTransactionModalOpen,
     closeNewTransactionModal,
-    newTransactionType
+    newTransactionType,
+    register,
+    errors,
+    control,
+    handleSubmit,
+    accounts
   }
 }
