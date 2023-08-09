c7dfcebe3c97144355f0444d0c19ade2967a6604
MateusJSouza <mateusjdev@gmail.com>
2023-08-08 21:14:54 -0300

    ✨ feat: creation of new account modal and useEditAccountModalController

---
+++ frontend/src/view/pages/Dashboard/components/modals/EditAccountModal/index.tsx
@@ -0,0 +1,94 @@
+import { Input } from "../../../../../components/Input";
+import { Modal } from "../../../../../components/Modal";
+import { Select } from "../../../../../components/Select";
+import { InputCurrency } from "../../../../../components/InputCurrency";
+import { ColorsDropdownInput } from "../../../../../components/ColorsDropdownInput";
+import { Button } from "../../../../../components/Button";
+import { Controller } from "react-hook-form";
+import { useEditAccountModal } from "./useEditAccountModal";
+
+export function EditAccountModal() {
+  const {
+    isEditAccountModalOpen,
+    closeEditAccountModal,
+    errors,
+    handleSubmit,
+    register,
+    control,
+    isLoading
+  } = useEditAccountModal()
+
+  return (
+    <Modal title="Editar Conta" open={isEditAccountModalOpen} onClose={closeEditAccountModal}>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <span className="text-gray-600 text-xs tracking-[-0.5px]">Saldo inicial</span>
+          <div className="flex items-center gap-2">
+            <span className="text-gray-600 text-lg tracking-[-0.5px]">R$</span>
+            <Controller
+              control={control}
+              name="initialBalance"
+              defaultValue="0"
+              render={({ field: { onChange, value } }) => (
+                <InputCurrency
+                  value={value}
+                  error={errors.initialBalance?.message}
+                  onChange={onChange}
+                />
+              )}
+            />
+          </div>
+        </div>
+
+        <div className="mt-10 flex flex-col gap-4">
+          <Input
+            type="text"
+            placeholder="Nome da Conta"
+            error={errors.name?.message}
+            {...register('name')}
+          />
+
+          <Controller
+            control={control}
+            name="type"
+            defaultValue="CASH"
+            render={({ field: { onChange, value } }) => (
+              <Select
+                placeholder="Tipo"
+                error={errors.type?.message}
+                onChange={onChange}
+                value={value}
+                options={[
+                  { value: 'CHECKING', label: 'Conta Corrente' },
+                  { value: 'INVESTMENT', label: 'Investimentos' },
+                  { value: 'CASH', label: 'Dinheiro Físico' },
+                ]}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="color"
+            defaultValue=""
+            render={({ field: { onChange, value } }) => (
+              <ColorsDropdownInput
+                error={errors.color?.message}
+                onChange={onChange}
+                value={value}
+              />
+            )}
+          />
+        </div>
+
+        <Button
+          type="submit"
+          className="w-full mt-6"
+          isLoading={isLoading}
+        >
+          Salvar
+        </Button>
+      </form>
+    </Modal>
+  )
+}
---
+++ frontend/src/view/pages/Dashboard/components/modals/EditAccountModal/useEditAccountModal.ts
@@ -0,0 +1,70 @@
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useDashboard } from '../../DashboardContext/useDashboard'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { toast } from 'react-hot-toast'
+import { bankAccountsService } from '../../../../../../app/services/bankAccountsService'
+import { currencyStringToNumber } from '../../../../../../app/utils/currencyStringToNumber'
+
+const schema = z.object({
+  initialBalance: z.union([
+    z.string().nonempty('Saldo inicial é obrigatório'),
+    z.number()
+  ]),
+  name: z.string().nonempty('Nome da conta é obrigatório'),
+  type: z.enum(['INVESTMENT', 'CHECKING', 'CASH']),
+  color: z.string().nonempty('Cor é obrigatória'),
+})
+
+type FormData = z.infer<typeof schema>
+
+export function useEditAccountModal() {
+  const { isEditAccountModalOpen, closeEditAccountModal, accountBeingEdited } = useDashboard()
+
+  const {
+    register,
+    handleSubmit: hookFormSubmit,
+    formState: { errors },
+    control,
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      color: accountBeingEdited?.color,
+      name: accountBeingEdited?.name,
+      type: accountBeingEdited?.type,
+      initialBalance: accountBeingEdited?.initialBalance,
+    }
+  })
+
+  const queryClient = useQueryClient();
+  const { mutateAsync, isLoading } = useMutation(bankAccountsService.update);
+
+  const handleSubmit = hookFormSubmit(async (data) => {
+    try {
+      await mutateAsync({
+        ...data,
+        initialBalance: currencyStringToNumber(data.initialBalance),
+        id: accountBeingEdited!.id // Non-null assertion
+      })
+
+      queryClient.invalidateQueries({
+        queryKey: ['bankAccounts']
+      });
+      toast.success('A conta foi editada com sucesso!');
+      closeEditAccountModal();
+    } catch {
+      toast.error('Erro ao salvar as alterações!')
+    }
+  })
+
+  return {
+    isEditAccountModalOpen,
+    closeEditAccountModal,
+    register,
+    errors,
+    handleSubmit,
+    control,
+    isLoading,
+  }
+}
