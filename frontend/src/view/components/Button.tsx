d974c54ba369863a4209621b1ffda518aae18029
MateusJSouza <mateusjdev@gmail.com>
2023-08-09 10:03:35 -0300

    🔧 refactor: adding color variants to the Button component

--- frontend/src/view/components/Button.tsx
+++ frontend/src/view/components/Button.tsx
@@ -5,15 +5,18 @@ import { Spinner } from "./Spinner";
 
 interface ButtonProps extends ComponentProps<'button'> {
   isLoading?: boolean;
+  variant?: 'danger' | 'ghost';
 }
 
-export function Button({ className, isLoading, disabled, children, ...props }: ButtonProps) {
+export function Button({ className, variant, isLoading, disabled, children, ...props }: ButtonProps) {
   return (
     <button
       {...props}
       disabled={disabled ?? isLoading}
       className={cn(
         'bg-teal-900 hover:bg-teal-800 disabled:bg-transparent disabled:text-gray-400 disabled:cursor-not-allowed px-6 h-12 rounded-2xl font-medium text-white tracking-[-0.5px] transition-all flex items-center justify-center',
+        variant === 'danger' && 'bg-red-900 hover:bg-red-800',
+        variant === 'ghost' && 'bg-transparent text-gray-800 border border-gray-800 hover:bg-gray-800/5',
         className,
       )}
     >
