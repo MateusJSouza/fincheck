f5d17fd04138453bdd98b8aea9e228b4486a0304
MateusJSouza <mateusjdev@gmail.com>
2023-08-09 10:03:06 -0300

    ✨ feat: creation of the delete method for deleting an account

--- frontend/src/app/services/bankAccountsService/index.ts
+++ frontend/src/app/services/bankAccountsService/index.ts
@@ -1,9 +1,11 @@
 import { create } from "./create";
+import { remove } from "./delete";
 import { getAll } from "./getAll";
 import { update } from "./update";
 
 export const bankAccountsService = {
   create,
   getAll,
-  update
+  update,
+  remove
 }
