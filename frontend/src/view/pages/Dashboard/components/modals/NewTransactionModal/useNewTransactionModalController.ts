a7923d0d907a73353f546e243707536df4848832
MateusJSouza <mateusjdev@gmail.com>
2023-08-09 12:00:39 -0300

    ✨ feat: creation of new transaction form submit function

--- frontend/src/view/pages/Dashboard/components/modals/NewTransactionModal/useNewTransactionModalController.ts
+++ frontend/src/view/pages/Dashboard/components/modals/NewTransactionModal/useNewTransactionModalController.ts
@@ -5,6 +5,10 @@ import { useForm } from "react-hook-form";
 import { useBankAccounts } from "../../../../../../app/hooks/useBankAccounts";
 import { useCategories } from "../../../../../../app/hooks/useCategories";
 import { useMemo } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { transactionsService } from "../../../../../../app/services/transactionsService";
+import { toast } from "react-hot-toast";
+import { currencyStringToNumber } from "../../../../../../app/utils/currencyStringToNumber";
 
 const schema = z.object({
   value: z.string().nonempty('Informe o valor'),
@@ -24,16 +28,43 @@ export function useNewTransactionModalController() {
     handleSubmit: hookFormSubmit,
     formState: { errors },
     control,
+    reset
   } = useForm<FormData>({
     resolver: zodResolver(schema),
   })
 
-  const handleSubmit = hookFormSubmit(data => {
-    console.log(data)
-  })
-
+  const queryClient = useQueryClient()
   const { accounts } = useBankAccounts();
   const { categories: categoriesList } = useCategories();
+  const { isLoading, mutateAsync } = useMutation(transactionsService.create)
+
+  const handleSubmit = hookFormSubmit(async data => {
+    try {
+      await mutateAsync({
+        ...data,
+        value: currencyStringToNumber(data.value),
+        type: newTransactionType!,
+        date: data.date.toISOString()
+      })
+
+      queryClient.invalidateQueries({
+        queryKey: ['transactions']
+      });
+      toast.success(
+        newTransactionType === 'EXPENSE'
+          ? 'Despesa cadastrada com sucesso!'
+          : 'Receita cadastrada com sucesso!'
+      );
+      closeNewTransactionModal();
+      reset();
+    } catch {
+      toast.error(
+        newTransactionType === 'EXPENSE'
+          ? 'Erro ao cadastrar a despesa!'
+          : 'Erro ao cadastrar a receita!'
+      )
+    }
+  })
 
   const categories = useMemo(() => {
     return categoriesList.filter(category => category.type === newTransactionType)
@@ -49,5 +80,6 @@ export function useNewTransactionModalController() {
     handleSubmit,
     accounts,
     categories,
+    isLoading
   }
 }
