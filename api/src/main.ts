3522c558137efcccf9635046ddff525656dd7b2b
MateusJSouza <mateusjdev@gmail.com>
2023-09-06 15:28:53 -0300

    ✨ feat: add env variables to prod URL and dev URL

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,12 +3,13 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { env } from './shared/config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: [env.feDevURL, env.feProdURL],
     credentials: true,
   });
 
--- api/src/shared/config/env.ts
+++ api/src/shared/config/env.ts
@@ -10,11 +10,22 @@ class Env {
   @IsNotEmpty()
   @NotEquals('unsecure_jwt_secret')
   jwtSecret: string;
+
+  @IsString()
+  @IsNotEmpty()
+  feDevURL: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @NotEquals('unsecure_prod_url')
+  feProdURL: string;
 }
 
 export const env: Env = plainToInstance(Env, {
   dbURL: process.env.DATABASE_URL,
   jwtSecret: process.env.JWT_SECRET,
+  feDevURL: process.env.FRONT_END_DEV_URL,
+  feProdURL: process.env.FRONT_END_PROD_URL,
 });
 
 const errors = validateSync(env);
